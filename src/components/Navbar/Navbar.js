7aa165458b642cf0e7c5482f120762b682b614ee
Shounoop <hmtuan9a@gmail.com>
2022-08-08 14:42:57 +0700

    Empty README.md

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,16 @@ import { todosSelector } from '../../store/reducer/todosSlice'
 function Navbar() {
   const todos = useSelector(todosSelector)
 
-  return (<div className="navbar">
-    <h1>My Redux App Todos</h1>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Total Todos: {todos.length}</li>
-    </ul>
-  </div>);
+  return (
+    <div className="navbar">
+      <h1>My Redux App Todos</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Total Todos: {todos.length}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
